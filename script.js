2fb022251fc92dfa6191f45a583f7249ecba8efc
Lucas Henrique <170979180+OHenriqueDev@users.noreply.github.com>
2025-06-17 12:23:30 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,28 @@
+let ingredientes = [];
+let custos = []
+
+document.getElementById("envio").addEventListener("click", function(event) {
+    event.preventDefault();
+
+    let ingrediente = document.getElementById("ingrediente").value;
+    let preco = parseFloat(document.getElementById("preco").value);
+    let quantidade = parseFloat(document.getElementById("quantidade").value);
+    let quantia = parseFloat(document.getElementById("quantidadereceita").value);
+
+    let custoreceita = (preco / quantidade) * quantia;
+
+    ingredientes.push({
+        nome: ingrediente
+    })
+    custos.push(custoreceita);
+
+    let custo = 0;
+
+    for (var i=0; i < custos.length; i++) {
+        custo += custos[i];
+    }
+    console.log(ingredientes);
+    document.getElementById("addingrediente").innerText = "O ingrediente '" + ingrediente + "' custou: R$" + custoreceita.toFixed(2) + "💸";
+
+    document.getElementById("resultado").innerText = "A receita custou: R$ "+ custo.toFixed(2) + "💸";
+});
